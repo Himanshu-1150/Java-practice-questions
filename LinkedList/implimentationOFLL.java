095e31307fd25208abbbb4a9f38774dbf1f594da
Himanshu-1150 <rajnirajni3307@gmail.com>
2024-12-27 09:26:00 +0530

    Implementation of LL add Element at first

---
+++ LinkedList/implimentationOFLL.java
@@ -0,0 +1,32 @@
+// package LinkedList;
+
+public class implimentationOFLL {
+  public static class Node {
+    int data;
+    Node next;
+    public Node (int data){
+      this.data = data;
+      this.next = null;
+
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst (int data){
+      // step1 -> create new node
+      Node newNode = new Node (data);
+      if (head == null){
+        head = tail = newNode;
+        return;
+      }
+      // step2 -> newNode next = head 
+      newNode.next = head; // link
+      // step3 -> head = newNode
+       head = newNode;
+    }
+  } 
+  
+  public static void main (String args[]){
+    System.out.println("Implimantation of linked list");
+    
+  }
+}
