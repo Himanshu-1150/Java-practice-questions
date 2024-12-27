5ef01e3832cb1664ed900cce321d20b56fce01e6
Himanshu-1150 <rajnirajni3307@gmail.com>
2024-12-27 09:34:18 +0530

    add last in Linked List

--- LinkedList/implimentationOFLL.java
+++ LinkedList/implimentationOFLL.java
@@ -7,24 +7,35 @@ public class implimentationOFLL {
     public Node (int data){
       this.data = data;
       this.next = null;
-
-    }
-    public static Node head;
-    public static Node tail;
-    public void addFirst (int data){
-      // step1 -> create new node
-      Node newNode = new Node (data);
-      if (head == null){
-        head = tail = newNode;
-        return;
-      }
-      // step2 -> newNode next = head 
-      newNode.next = head; // link
-      // step3 -> head = newNode
-       head = newNode;
-    }
+    }  
   } 
+  // add at First
+  public void addFirst (int data){
+    // step1 -> create new node
+    Node newNode = new Node (data);
+    if (head == null){
+      head = tail = newNode;
+      return;
+    }
+    // step2 -> newNode next = head 
+    newNode.next = head; // link
+    // step3 -> head = newNode
+     head = newNode;
+  }
+
+  public static Node head;
+  public static Node tail;
   
+  // add Last in LL
+  public void addLast (int data){
+    Node newNode = new Node(data);
+    if (head == null){
+      head = tail = newNode;
+      return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+  }
   public static void main (String args[]){
     System.out.println("Implimantation of linked list");
     
