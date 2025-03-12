92fb9f4cb889e85e5734cc9831478d5d11fccc4e
Himanshu-1150 <rajnirajni3307@gmail.com>
2025-03-12 19:28:40 +0530

    Diameter of a Tree using  O(n^2) algorithm

---
+++ Binary Tree/DiameterOfTree.java
@@ -0,0 +1,40 @@
+public class DiameterOfTree {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(diameter(root));
+    }
+
+    private static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int leftDiameter = diameter(root.left);
+        int rightDiameter = diameter(root.right);
+        return Math.max(leftHeight + rightHeight + 1, Math.max(leftDiameter, rightDiameter));
+    }
+
+    private static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+}
