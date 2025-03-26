4ede33b334bd4028424c10ce4f9215ab13660259
Himanshu-1150 <parjapatihim1150@gmail.com>
2025-03-26 22:52:41 +0530

    problems of binary search tree

--- Binary Search Tree/Mirror.java
+++ Binary Search Tree/Mirror.java
@@ -30,6 +30,16 @@ public class Mirror {
     preorder(root.right);
     
   }
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+        return null;
+    }
+    TreeNode left = invertTree(root.left);
+    TreeNode right = invertTree(root.right);
+    root.left = right;
+    root.right = left;
+    return root;
+}
   public static void main (String args[] ) {
     System.out.println("Mirror of a binary tree");
     Node root = new Node (8);
